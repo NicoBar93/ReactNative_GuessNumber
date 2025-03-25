4e5e0112015d289f1dc174d3c321ca9603c85e39
Nicola Barbaro <barbaronico93@gmail.com>
2025-03-25 18:51:48 +0100

    Starting with Game Logic

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <LinearGradient
+        colors={["#4e0329", "#ddb52f"]}
+        style={styles.rootContainer}
+      >
+        <ImageBackground
+          source={require("./assets/images/background.png")}
+          resizeMode={"cover"}
+          style={styles.rootContainer}
+          imageStyle={styles.backgroundImage}
+        >
+          <StartGameScreen />
+        </ImageBackground>
+      </LinearGradient>
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
+});
